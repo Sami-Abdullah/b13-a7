70676ccc7c1e412686243a24d8decccf10970a50
Sami Abdullah <sami38596@gmailcom>
2026-04-17 06:48:37 +0600

    made navbar and footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,9 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -18,11 +22,20 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="en" data-theme="light" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
+      <body className="min-h-screen flex flex-col">
+
+
+        <div className="">
+          <Navbar></Navbar>
+          {children}
+          <Footer></Footer>
+        </div>
+
+
+
+
+      </body>
     </html>
   );
 }
